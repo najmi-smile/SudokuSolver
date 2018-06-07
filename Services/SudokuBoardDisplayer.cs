fc27c1d9fd6be986b0d64cdab21448caa7612853
Tahir Raza <tahir.raza@accesspay.com>
2018-06-07 15:22:57 +0100

    Add a displayer for the board to be displayed on console

---
+++ Services/SudokuBoardDisplayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace SudokuSolver.Services
+{
+    class SudokuBoardDisplayer
+    {
+        public void Display(string title, int[,] sudokuBoard)
+        {
+            if (!title.Equals(string.Empty)) WriteLine("{0}{1}", title, Environment.NewLine);
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                Write("|");
+                for(int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    WriteLine($"{sudokuBoard[row, col]}|");
+                }
+                WriteLine();
+            }
+            WriteLine();
+
+        }
+    }
+}
